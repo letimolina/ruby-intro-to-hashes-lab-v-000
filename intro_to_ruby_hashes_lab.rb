2d915f4fb7b5d6ae7a82db16540b8e98311c4fbf
Leticia Molina <letimolina@gmail.com>
2018-05-27 20:52:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,22 @@ monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
+	      :reading_railroad=>
+      {
+        
+      },
+      :pennsylvania_railroad=>
+      {
+        
+      },
+      :b_and_o_railroad=>
+      {
+       
+      },
+      :shortline=>
+      {
+        
+      }
 	      
 	    },
 	    :rent_in_dollars =>{
