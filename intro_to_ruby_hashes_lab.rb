876ca82d905e9df3847aa47dd1c7d9129c625082
Leticia Molina <letimolina@gmail.com>
2018-05-27 20:51:36 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,10 @@ monopoly = {
 	      
 	    },
 	    :rent_in_dollars =>{
-	      
+	       :one_piece_owned=>25,
+         :two_pieces_owned=>50,
+         :three_pieces_owned=>100,
+         :four_pieces_owned=>200
 	    }
 	  }
 	}	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
